df426cbd66718dbb919e7393a045e898db924397
Marcia-Free <MarciaFreeman2011@gmail.com>
2021-05-10 11:30:15 -0500

    Lesson 2 Done. Player movement and animations.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,21 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float _speed = 3;
+    [SerializeField] private float _jumpForce = 400;
 
     // Update is called once per frame
     void Update()
     {
-        var horizontal = Input.GetAxis("Horizontal");
+        var horizontal = Input.GetAxis("Horizontal") * _speed;
         var rigidbody2D = GetComponent<Rigidbody2D>();
-
         rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
+        Debug.Log($"Velocity = {rigidbody2D.velocity}");
+
+        var animator = GetComponent<Animator>();
+        bool walking = horizontal != 0;
+        animator.SetBool("Walk", walking);        
+
+        if (horizontal != 0)
+        {
+            var spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.flipX = horizontal < 0;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            rigidbody2D.AddForce(Vector2.up * _jumpForce);
+        }
+
     }
 }
